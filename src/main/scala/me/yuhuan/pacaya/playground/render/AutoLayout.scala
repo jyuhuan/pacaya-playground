6d508d5094fcd49a08bfa8707aada5d10ad666db
Yuhuan Jiang <jyuhuan@gmail.com>
2016-10-19 14:46:29 -0400

    proposed to use LBP to solve the automatic layout problem of factor graphs.

--- src/main/scala/me/yuhuan/pacaya/playground/render/AutoLayout.scala
+++ src/main/scala/me/yuhuan/pacaya/playground/render/AutoLayout.scala
@@ -1,10 +1,12 @@
 package me.yuhuan.pacaya.playground.render
 
+import edu.jhu.pacaya.gm.model._
+
 /**
   * An automatic layout generator for factor graphs.
   *
   * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 10/14/2016.
   */
-class AutoLayout {
+class AutoLayout(fg: FactorGraph, groups: Iterable[Iterable[Factor]]) {
   //TODO: use LBP to solve this constraint satisfaction problem!
 }
